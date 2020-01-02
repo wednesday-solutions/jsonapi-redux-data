24f59417335bb3ff81edd646e099d7194f60a3b5
Mac <mohammed.ali.chherawalla@gmail.com>
2020-01-03 03:11:38 +0530

    updating readme

---
+++ src/reducers/README.md
@@ -0,0 +1,43 @@
+# JsonApi Redux Data
+
+- Provides methods to make api calls to your JSONApi compliant backend and updates the redux store as well. 
+
+- Combines the data so it is easily accessible 
+
+- Setup in 3 easy steps 
+
+    - Update the reducers / rootReducers like this 
+        ```
+        ...
+        import { jsonApiReducer } from 'jsonapi-redux-data'
+        ...
+        const rootReducer = combineReducers({
+            ...,
+            api: jsonApiReducer,
+            ...
+        })
+        ...
+
+        ```
+    - Create the api client preferrably in the app.js 
+        ```
+        ...
+        import { createApiClientWithTransform } from './jsonapi-redux-data'
+        ...
+        // Create redux store with history
+        const initialState = {};
+        const store = configureStore(initialState, history);
+        ...
+
+        createApiClientWithTransform('<base-url>', store)
+        ...
+        ```
+    - Make api call easily and from anywhere 
+        ```
+        ...
+        import { getApi, createReducerActions } from '../../jsonapi-redux-data'
+        ...
+
+        getApi({ pathname: '<pathname>', include: '<include-string>' })
+        ...
+        ```
--- src/reducers/jsonApiReducer.js
+++ src/reducers/jsonApiReducer.js
@@ -1,22 +1,19 @@
-import { createActions } from 'reduxsauce';
-import { fromJS } from 'immutable';
-import merge from 'deepmerge';
-import produce from 'immer';
-import { overwriteMerge } from '../utils';
+import { createActions } from 'reduxsauce'
+import { fromJS } from 'immutable'
+import merge from 'deepmerge'
+import produce from 'immer'
+import { overwriteMerge } from '../utils'
 
-const REQUEST_PAYLOAD = ['requestPayload'];
-const RESPONSE_PAYLOAD = ['responsePayload'];
+const RESPONSE_PAYLOAD = ['responsePayload']
 export const createReducerActions = () =>
   createActions({
-    requestGetApi: REQUEST_PAYLOAD,
-    requestPostApi: REQUEST_PAYLOAD,
-    requestPutApi: REQUEST_PAYLOAD,
-    requestPatchApi: REQUEST_PAYLOAD,
-    requestDeleteApi: REQUEST_PAYLOAD,
     successApi: RESPONSE_PAYLOAD
-  });
-export const { Types: jsonApiTypes, Creators: jsonApiCreators } = createReducerActions();
-export const initialState = fromJS({});
+  })
+export const {
+  Types: jsonApiTypes,
+  Creators: jsonApiCreators
+} = createReducerActions()
+export const initialState = fromJS({})
 
 /* eslint-disable default-case, no-param-reassign */
 export const jsonApiReducer = (state = initialState, action) =>
@@ -27,9 +24,9 @@ export const jsonApiReducer = (state = initialState, action) =>
           merge.all([state.toJS(), action.responsePayload], {
             arrayMerge: overwriteMerge
           })
-        );
+        )
     }
-    return state;
-  });
+    return state
+  })
 
-export default jsonApiReducer;
+export default jsonApiReducer
